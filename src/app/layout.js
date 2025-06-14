a3e4496e2c6e7dd9b71a222ea1a3fbb2a4085a76
Collins Dada <140630132+Contractor-x@users.noreply.github.com>
2025-06-14 22:27:38 +0100

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,1 +1,6 @@
+// Example: src/app/layout.js
+import { AuthProvider } from '../context/AuthContext';
 
+export default function RootLayout({ children }) {
+  return <AuthProvider>{children}</AuthProvider>;
+}
