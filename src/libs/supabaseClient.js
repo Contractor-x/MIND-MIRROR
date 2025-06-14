2c06a47228f0723a2ea7d67e6f66102f40f50bab
Collins Dada <140630132+Contractor-x@users.noreply.github.com>
2025-06-14 20:34:52 +0100

    Update supabaseClient.js

--- src/libs/supabaseClient.js
+++ src/libs/supabaseClient.js
@@ -1,1 +1,5 @@
+import { createClient } from '@supabase/supabase-js'
 
+const supabaseUrl = 'https://your-project.supabase.co'
+const supabaseKey = 'your-anon-public-key'
+export const supabase = createClient(supabaseUrl, supabaseKey)
